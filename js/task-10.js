a57ba748a18c9ba772ff6e99fd88aab133642231
AlonaMaliarchyk <alenasymorot@gmail.com>
2022-11-12 19:29:52 +0400

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,40 +4,79 @@ function getRandomHexColor() {
 
 const createBtn = document.querySelector('#controls button[data-create]');
 const destroyBtn = document.querySelector('#controls button[data-destroy]');
+const divBoxes = document.querySelector("#boxes");
+const input = document.querySelector('#controls input');
+ 
 
-createBtn.addEventListener('click', () => {
-  const input = document.querySelector('#controls input');
-  createBoxes(input.value);
-})
-
-destroyBtn.addEventListener('click', () => {
-  destroyBoxes();
-})
-
+function rendomRGB() {
+  const red = Math.round(Math.random(0, 1) * 255);
+  const green = Math.round(Math.random(0, 1) * 255);
+  const blue = Math.round(Math.random(0, 1) * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
+ }
 function createBoxes(amount) {
-  let height = 30;
-  let width = 30;
-  
-  const divBoxes = document.querySelector("#boxes");
+  let size = 30;
+  amount = Number(input.value);
+  console.log(amount);
+  console.log(typeof amount);
+  const boxArr = [];
   for (let i = 1; i <= amount; i += 1){
-    const element = document.createElement("div");
-    element.style.width = width + 'px';
-    element.style.height = height + 'px';
-    element.style.backgroundColor = getRandomHexColor();
-    element.style.marginBottom = '10px';
-    divBoxes.appendChild(element);
-    height += 10;
-    width += 10;
+    let growingSize = size + i * 10;
+    let boxElement = `<div style=" height:${growingSize}px; width:${growingSize}px; background-color:${rendomRGB()}; margin: 5px;"></div>`;
+    boxArr.push(boxElement);
+    console.log(size);
+    console.log(boxArr);
   }
+  divBoxes.insertAdjacentHTML('beforebegin', boxArr.join(""));
 }
 
 function destroyBoxes() {
-  const divBoxes = document.querySelector("#boxes");
-
-  while (divBoxes.children.length > 0) { 
-    divBoxes.firstChild.remove();
-  }
+  input.value = "";
+  divBoxes.innerHTML = "";
 }
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+//-------------------------------------------------------
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const createBtn = document.querySelector('#controls button[data-create]');
+// const destroyBtn = document.querySelector('#controls button[data-destroy]');
+
+// createBtn.addEventListener('click', () => {
+//   const input = document.querySelector('#controls input');
+//   createBoxes(input.value);
+// })
+
+// destroyBtn.addEventListener('click', () => {
+//   destroyBoxes();
+// })
+
+// function createBoxes(amount) {
+//   let height = 30;
+//   let width = 30;
+  
+//   const divBoxes = document.querySelector("#boxes");
+//   for (let i = 1; i <= amount; i += 1){
+//     const element = document.createElement("div");
+//     element.style.width = width + 'px';
+//     element.style.height = height + 'px';
+//     element.style.backgroundColor = getRandomHexColor();
+//     element.style.marginBottom = '10px';
+//     divBoxes.appendChild(element);
+//     height += 10;
+//     width += 10;
+//   }
+// }
+
+// function destroyBoxes() {
+//   const divBoxes = document.querySelector("#boxes");
+
+//   while (divBoxes.children.length > 0) { 
+//     divBoxes.firstChild.remove();
+//   }
+// }
 
 // Завдання 10(виконувати не обов'язково)
 // Напиши скрипт створення і очищення колекції елементів. 
